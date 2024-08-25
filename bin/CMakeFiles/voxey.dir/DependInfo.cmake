5f2b008a82adc00f3d09f5221609c49772172079
Filip Ryan <filipryanbusinessmail@gmail.com>
2024-08-25 13:52:22 +0200

    added cmake build configurations

---
+++ bin/CMakeFiles/voxey.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/ext/glad.c" "CMakeFiles/voxey.dir/src/ext/glad.c.obj" "gcc" "CMakeFiles/voxey.dir/src/ext/glad.c.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/core/application.cpp" "CMakeFiles/voxey.dir/src/core/application.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/core/application.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/core/engine.cpp" "CMakeFiles/voxey.dir/src/core/engine.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/core/engine.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/core/logger.cpp" "CMakeFiles/voxey.dir/src/core/logger.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/core/logger.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/input/inputManager.cpp" "CMakeFiles/voxey.dir/src/input/inputManager.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/input/inputManager.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/main.cpp" "CMakeFiles/voxey.dir/src/main.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/main.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/rendering/renderer.cpp" "CMakeFiles/voxey.dir/src/rendering/renderer.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/rendering/renderer.cpp.obj.d"
+  "C:/Users/Filip Ryan/Downloads/important/vscode/voxey/src/utils/time.cpp" "CMakeFiles/voxey.dir/src/utils/time.cpp.obj" "gcc" "CMakeFiles/voxey.dir/src/utils/time.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/voxey.dir/src/rendering/renderer.cpp.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/voxey.dir/src/rendering/renderer.cpp.obj: \
+ C:\Users\Filip\ Ryan\Downloads\important\vscode\voxey\src\rendering\renderer.cpp \
+ C:\Users\Filip\ Ryan\Downloads\important\vscode\voxey\src\rendering\renderer.h \
+ C:/Users/Filip\ Ryan/Downloads/important/vscode/voxey/include/glad/glad.h \
+ C:/Users/FILIPR~1/DOWNLO~1/IMPORT~1/vscode/voxey/include/KHR/khrplatform.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdint.h \
+ C:/msys64/ucrt64/include/stdint.h C:/msys64/ucrt64/include/crtdefs.h \
+ C:/msys64/ucrt64/include/corecrt.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/vadefs.h \
+ C:/msys64/ucrt64/include/sdks/_mingw_ddk.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stddef.h \
+ C:/msys64/ucrt64/include/stddef.h \
+ C:/Users/Filip\ Ryan/Downloads/important/vscode/voxey/include/GLFW/glfw3.h
